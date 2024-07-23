d92a212eb3c6f0315e0c8ea16b5fab2ec0391956
Ardón <josueardon.developer@gmail.com>
2024-07-22 20:23:36 -0600

    feat(add_sale): add bottom sheet
    - Created bottom sheet for sale adding

--- app/src/main/java/com/ardondev/tiendita/presentation/util/GeneralComponents.kt
+++ app/src/main/java/com/ardondev/tiendita/presentation/util/GeneralComponents.kt
@@ -2,11 +2,24 @@ package com.ardondev.tiendita.presentation.util
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import com.ardondev.tiendita.R
 
 @Composable
 fun ErrorView(message: String) {
@@ -28,4 +41,52 @@ fun LoadingView() {
     ) {
         CircularProgressIndicator()
     }
+}
+
+@Composable
+fun QuantityControl(
+    onMinus: () -> Unit,
+    onPlus: () -> Unit,
+    quantity: Int,
+    stock: Int,
+    modifier: Modifier,
+    value: MutableState<String>,
+) {
+    OutlinedTextField(
+        value = value.value,
+        onValueChange = { value.value = it },
+        label = { Text(stringResource(R.string.txt_quantity)) },
+        leadingIcon = {
+            IconButton(onClick = {
+                onMinus()
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Remove,
+                    contentDescription = "Minus",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                onPlus()
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Plus",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        },
+        textStyle = TextStyle(
+            textAlign = TextAlign.Center
+        ),
+        supportingText = {
+            Text("Stock: ${stock - quantity}")
+        },
+        modifier = modifier
+            .focusProperties {
+                canFocus = false
+            }
+    )
 }
